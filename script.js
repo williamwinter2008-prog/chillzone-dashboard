3a5c38f3ee2ad1fb29bb3e3427e55f6e33a99d07
williamwinter2008-prog <williamwinter2008@gmail.com>
2026-08-03 22:32:18 +0100

    Add theme switch script

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const button = document.getElementById("theme-toggle");
 
+if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light");
+    button.textContent = "☀️";
+}
+
+button.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+
+    if (document.body.classList.contains("light")) {
+        localStorage.setItem("theme", "light");
+        button.textContent = "☀️";
+    } else {
+        localStorage.setItem("theme", "dark");
+        button.textContent = "🌙";
+    }
+});
