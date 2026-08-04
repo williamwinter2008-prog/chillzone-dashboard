41519b6386ae35390de7ec9a690f31a30c994149
williamwinter2008-prog <williamwinter2008@gmail.com>
2026-08-04 08:39:02 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(cors());
+
+
+const client = new Client({
+
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers
+    ]
+
+});
+
+
+
+let botStatus = "Offline";
+let memberCount = 0;
+
+
+
+client.once("ready", () => {
+
+    console.log(`Logged in as ${client.user.tag}`);
+
+    botStatus = "Online";
+
+
+    const guild = client.guilds.cache.first();
+
+
+    if(guild){
+
+        memberCount = guild.memberCount;
+
+    }
+
+});
+
+
+
+app.get("/api/status", (req,res)=>{
+
+
+res.json({
+
+    status: botStatus,
+    members: memberCount
+
+});
+
+
+});
+
+
+
+app.listen(3000, ()=>{
+
+console.log("Dashboard server running on port 3000");
+
+});
+
+
+
+client.login("MTUzMjg3MDI1MDQ4MjIzNzUzMA.GUeVHY.c0rAq60oIw1-SN2q90MzJ3gHiKQVX3M2jSoNbQ");
